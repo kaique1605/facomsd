b2611a00761d56c115e88b35b83d963cab7d4f22
kaique1605 <kaiqueribeiro.01@hotmail.com>
2018-12-12 12:39:59 -0200

    inicio trab 3

--- trabalhoSD/src/main/java/servidor/Node.java
+++ trabalhoSD/src/main/java/servidor/Node.java
@@ -11,48 +11,65 @@ import io.grpc.ServerBuilder;
 
 @Component(immediate = true)
 public class Node {
-//  private final int port = 5000;
-	private Server server;
-//  private BindableService greeterService;
-
-//	@Activate
-//	public void activate(String andress, int port, BigInteger id, BigInteger minKey, BigInteger maxKey, int antecessor,
-//			int sucessor) throws InterruptedException {
-//		start(andress, port, id, minKey, maxKey, antecessor, sucessor);
-//	}
-
-	public void start(String andress, int port, BigInteger id, BigInteger minKey, BigInteger maxKey, int antecessor,
-			int sucessor) throws InterruptedException {
-		try {
-			ServerClass serverClass = new ServerClass(andress, port, id, minKey, maxKey, antecessor, sucessor);
-			server = ServerBuilder.forPort(serverClass.getFinger().getPort()).addService(serverClass).build();
-			server.start();
-			System.out.println("Server iniciado");
-			// server.awaitTermination();
-		} catch (IOException ex) {
-		}
-	}
-
-	public void start(BigInteger id) throws InterruptedException {
-		try {
-			ServerClass serverClass = new ServerClass(id);
-			Server server = ServerBuilder.forPort(serverClass.getFinger().getPort()).addService(serverClass).build();
-			server.start();
-			System.out.println("Server iniciado");
-			server.awaitTermination();
-		} catch (IOException ex) {
-		}
-	}
-
-//  @Reference
-//  public void setGreeterService(BindableService greeterService) {
-//    this.greeterService = greeterService;
-//  }
-
-	@Deactivate
-	public void deactivate() {
-		if (server != null) {
-			server.shutdownNow();
-		}
-	}
+  // private final int port = 5000;
+  private static Server server;
+  // private BindableService greeterService;
+  
+  // @Activate
+  // public void activate(String andress, int port, BigInteger id, BigInteger
+  // minKey, BigInteger maxKey, int antecessor,
+  // int sucessor) throws InterruptedException {
+  // start(andress, port, id, minKey, maxKey, antecessor, sucessor);
+  // }
+  public static void start(String andress, int port, BigInteger id, BigInteger minKey, BigInteger maxKey, int antecessor, int sucessor)
+      throws InterruptedException {
+    try {
+      ServerClass serverClass = new ServerClass(andress, port, id, minKey, maxKey, antecessor, sucessor);
+      server = ServerBuilder.forPort(serverClass.getFinger().getPort()).addService(serverClass).build();
+      server.start();
+      System.out.println("Server iniciado");
+      // server.awaitTermination();
+    } catch (IOException ex) {
+    }
+  }
+  
+  public static void start(BigInteger id) throws InterruptedException {
+    try {
+      ServerClass serverClass = new ServerClass(id);
+      Server server = ServerBuilder.forPort(serverClass.getFinger().getPort()).addService(serverClass).build();
+      server.start();
+      System.out.println("Server iniciado");
+      server.awaitTermination();
+    } catch (IOException ex) {
+    }
+  }
+  
+  // @Reference
+  // public void setGreeterService(BindableService greeterService) {
+  // this.greeterService = greeterService;
+  // }
+  @Deactivate
+  public void deactivate() {
+    if (server != null) {
+      server.shutdownNow();
+    }
+  }
+  
+  public static void main(String[] args) {
+    try {
+      if (args.length > 1) {
+        start(args[0], Integer.parseInt(args[1]), new BigInteger(args[2]), new BigInteger(args[3]), new BigInteger(args[4]),
+            Integer.parseInt(args[5]), Integer.parseInt(args[6]));
+      }
+      else {
+        start(new BigInteger(args[0]));
+      }
+    } catch (NumberFormatException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
 }
